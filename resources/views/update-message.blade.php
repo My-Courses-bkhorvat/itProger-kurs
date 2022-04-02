0382bd2428d5de222f85f9c63b89da4378980f65
bkhorvat <102609582+bkhorvat@users.noreply.github.com>
2022-04-02 10:31:29 +0300

    first commit

---
+++ resources/views/update-message.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title-block')Обновление записи@endsection
+
+@section('content')
+    <h1>Обновление записи</h1>
+
+
+    <form action="{{route('contact-update-submit', $data->id)}}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input type="text" name="name" value="{{ $data->name }}" placeholder="Введите имя" id="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Введите email</label>
+            <input type="text" name="email" value="{{ $data->email }}" placeholder="Введите email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="subject">Введите тему</label>
+            <input type="text" name="subject" value="{{ $data->subject }}" placeholder="Введите тему" id="subject" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" class="form-control" placeholder="Введите сообщение">{{ $data->message }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Обновить</button>
+    </form>
+@endsection
